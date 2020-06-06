78a33f31c1f041b952f6b2359798e9fd0702e42a
bagasys <37441075+bagasys@users.noreply.github.com>
2020-06-06 10:39:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './components/layouts/Navbar'
+import Users from './components/users/Users'
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Navbar/>
+        <div className="container">
+          <Users/>
+        </div>
+        
       </div>
     );
   }
