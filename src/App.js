920c713756bc2584c1f128b1bf7b003a41231319
bagasys <37441075+bagasys@users.noreply.github.com>
2020-06-06 11:09:59 +0700

    Make data centrallized in App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layouts/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
+
 class App extends React.Component {
+  // componentDidMount(){
+  //   axios.get('https://api.github.com/users').then(res => console.log(res.data))
+  // }
+  state = {
+    users: [],
+    loading: false,
+  }
+  async componentDidMount(){
+    this.setState({loading: true})
+    const res = await axios.get('https://api.github.com/users')
+    this.setState({users: res.data, loading: false})
+  }
+
   render(){
     return (
       <div className="App">
         <Navbar/>
         <div className="container">
-          <Users/>
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
         
       </div>
