f85379bb0fe6d2155f983c1d54eae6f8d288334e
bagasys <37441075+bagasys@users.noreply.github.com>
2020-06-07 00:40:56 +0700

    Update Navbar.js

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 const Navbar = ({icon, title}) => {
       return (
       <div className="navbar bg-primary">
         <h1> <i className={icon}> </i> {title}</h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
       </div>
     )
 }
